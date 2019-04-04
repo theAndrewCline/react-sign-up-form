b1bd5af04a18a05943571025f6fb2bb0a73aab00
Andrew Cline <andrew.cline77@gmail.com>
2019-04-04 07:50:53 -0500

    restructure folder content add SignInPage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
-import './App.css';
-import SignInCard from './components/SignInCard';
-import { Page } from './DefaultComponents'
+import React, { useState } from 'react'
+import './App.css'
+import SignInPage from './pages/SignInPage';
 
-class App extends Component {
-  render () {
-    return (
-      <Page>
-        <SignInCard />
-      </Page>
-    )
-  }
+function App () {
+  const [signedIn, setSignedIn] = useState(false)
+
+  return (
+    <div>
+      {signedIn ? <h1>Ready To Go</h1> : <SignInPage></SignInPage>}
+    </div>
+
+  )
 }
 
 export default App
---
+++ src/pages/SignInPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Page } from '../DefaultComponents';
+import SignInCard from '../components/SignInCard';
+
+function SignInPage () {
+  return (
+    <Page>
+      <SignInCard></SignInCard>
+    </Page>
+  )
+}
+
+export default SignInPage
