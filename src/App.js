954b032650ee6bf194a73a6d973e34f6e55875e1
Andrew Cline <andrew.cline77@gmail.com>
2019-04-04 12:25:06 -0500

    add nav bar

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 import SignInPage from './pages/SignInPage'
 import SignUpPage from './pages/SignUpPage'
 import HomePage from './pages/HomePage'
+import NavBar from './components/NavBar';
 
 function App () {
   const [signedIn, setSignedIn] = useState(false)
 
   return (
-    <Router>
-      <Route path='/' exact component={HomePage}></Route>
-      <Route path='/signin/' component={SignInPage}></Route>
-      <Route path='/signup/' component={SignUpPage}></Route>
-    </Router>
+    <>
+      <NavBar signedIn={signedIn}></NavBar>
+      <Router>
+        <Route path='/' exact component={HomePage}></Route>
+        <Route path='/signin/' component={SignInPage}></Route>
+        <Route path='/signup/' component={SignUpPage}></Route>
+      </Router>
+    </>
   )
 }
 
--- src/components/SignInCard.js
+++ src/components/SignInCard.js
@@ -17,9 +17,9 @@ function SignInCard () {
   return (
     <Card>
       <H1>Sign In</H1>
-      <Label for="email" ref={emailInput}>Email</Label>
+      <Label htmlFor="email" ref={emailInput}>Email</Label>
       <Input id="email"></Input>
-      <Label for="password">Password</Label>
+      <Label htmlFor="password">Password</Label>
       <Input type="password" id="password"></Input>
       <SubmitButton className="signin" primary>Sign In</SubmitButton>
       <A id="signup" href="/signup">Don't have an account? Sign Up Here</A>
